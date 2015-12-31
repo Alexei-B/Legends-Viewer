94f21e7f353b7ac8c6637fa49cf8e706172272ac
Alexei Barnes <ab@alexeibarnes.com>
2015-12-31 17:13:29 +0000

    Minor changes to map code.

--- LegendsViewer/Controls/Map/MapPanel.cs
+++ LegendsViewer/Controls/Map/MapPanel.cs
@@ -13,9 +13,10 @@ namespace LegendsViewer.Controls.Map
         public World World;
 
         public DwarfTabControl TabControl;
-        public Rectangle View;
+        public RectangleF View, Map, Tile;
         public Point MousePanStart, MouseClickLocation, MouseLocation;
-        public double Zoom = 1.0, PixelWidth = 1.0, PixelHeight = 1.0, ZoomChange = 0.15, ZoomMax = 10.0, ZoomMin = 0.2;
+        public double PixelWidth = 1.0, PixelHeight = 1.0;
+        public float MinWidth = 0.05F, MaxWidth = 1.5F, Aspect;
 
         public int YearMin, YearMax;
 
@@ -28,23 +29,87 @@ namespace LegendsViewer.Controls.Map
             Moduals = moduals.ToList();
         }
 
-        protected override void OnPaint(PaintEventArgs e)
+        public void AddModual(MapModual m)
         {
-            base.OnPaint(e);
+            Moduals.Add(m);
         }
 
-        private void Pan(int X, int Y)
+        public void SetView(Rectangle v)
+        {
+            View = v;
+            Refresh();
+        }
+
+        public void Pan(float X, float Y)
         {
             View.Offset(X, Y);
             Refresh();
         }
 
+        public void SetViewCenter(Point c)
+        {
+            Pan(c.X - (View.Left - View.Width / 2),
+                c.Y - (View.Top - View.Height / 2));
+        }
 
+        public void SetYear(int min, int max)
+        {
+            YearMin = min;
+            YearMax = max;
+            Refresh();
+        }
+
+        public void SetYear(int year)
+        {
+            SetYear(year, year);
+        }
+
+        public void SetWidth(float width)
+        {
+            View.Width = Math.Max(MinWidth, Math.Min(MaxWidth, width));
+            View.Height = View.Width * Aspect;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            foreach (MapModual m in Moduals)
+                m.Draw(e);
+
+            base.OnPaint(e);
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnMouseWheel(MouseEventArgs e)
+        {
+            base.OnMouseWheel(e);
+        }
     }
 
     class MapModual : Control
     {
+        public void Draw(PaintEventArgs e)
+        {
 
+        }
     }
 
     class MapBitmapModual : MapModual
